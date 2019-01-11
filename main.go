898adee715f79669ea98575cfeef3534e665bc5c
jmitchell <jmitchell@exzeo.com>
2019-01-11 15:36:10 -0500

    changes

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ package main
 
 import (
 	"bytes"
-	"context"
+	// "context"
 	"crypto/tls"
-	"encoding/base64"
+	// "encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -20,23 +20,16 @@ import (
 	"strconv"
 	"syscall"
 	"time"
-
-	"cloud.google.com/go/storage"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/cloudkms/v1"
-	"google.golang.org/api/option"
 )
 
 var (
 	vaultAddr     string
+	k8sHost       string
+	k8sPort       string
+	k8sAddr       string
 	checkInterval string
 	gcsBucketName string
 	httpClient    http.Client
-
-	kmsService *cloudkms.Service
-	kmsKeyId   string
-
-	storageClient *storage.Client
 
 	userAgent = fmt.Sprintf("vault-init/0.1.0 (%s)", runtime.Version())
 )
@@ -45,6 +38,22 @@ var (
 type InitRequest struct {
 	SecretShares    int `json:"secret_shares"`
 	SecretThreshold int `json:"secret_threshold"`
+}
+
+type secretMetadata struct {
+	name string
+}
+
+type secretData struct {
+	testSecret  string
+	testSecret2 string
+}
+
+type k8sSecretsRequest struct {
+	kind       string
+	apiVersion string
+	metadata   secretMetadata
+	data       secretData
 }
 
 // InitResponse holds a Vault init response.
@@ -71,9 +80,23 @@ type UnsealResponse struct {
 func main() {
 	log.Println("Starting the vault-init service...")
 
+	// Set kubernetes token as ENV variable
+    kubeToken, err := ioutil.ReadFile("file.txt") // just pass the file name
+    if err != nil {
+        fmt.Print(err)
+    }
+	os.Setenv("Token", string(kubeToken))
+
 	vaultAddr = os.Getenv("VAULT_ADDR")
 	if vaultAddr == "" {
-		vaultAddr = "https://127.0.0.1:8200"
+		// vaultAddr = "https://127.0.0.1:8200"
+		vaultAddr = "http://vault-dev.exzeo.io:8200"
+	}
+
+	k8sHost = os.Getenv("KUBERNETES_SERVICE_HOST")
+	k8sPort = os.Getenv("KUBERNETES_PORT_443_TCP_PORT")
+	if k8sAddr == "" {
+		k8sAddr = "https://"+k8sHost+k8sPort
 	}
 
 	checkInterval = os.Getenv("CHECK_INTERVAL")
@@ -88,40 +111,40 @@ func main() {
 
 	checkIntervalDuration := time.Duration(i) * time.Second
 
-	gcsBucketName = os.Getenv("GCS_BUCKET_NAME")
-	if gcsBucketName == "" {
-		log.Fatal("GCS_BUCKET_NAME must be set and not empty")
-	}
+	// gcsBucketName = os.Getenv("GCS_BUCKET_NAME")
+	// if gcsBucketName == "" {
+	// 	log.Fatal("GCS_BUCKET_NAME must be set and not empty")
+	// }
 
-	kmsKeyId = os.Getenv("KMS_KEY_ID")
-	if kmsKeyId == "" {
-		log.Fatal("KMS_KEY_ID must be set and not empty")
-	}
+	// kmsKeyId = os.Getenv("KMS_KEY_ID")
+	// if kmsKeyId == "" {
+	// 	log.Fatal("KMS_KEY_ID must be set and not empty")
+	// }
 
-	kmsCtx, kmsCtxCancel := context.WithCancel(context.Background())
-	defer kmsCtxCancel()
-	kmsClient, err := google.DefaultClient(kmsCtx, "https://www.googleapis.com/auth/cloudkms")
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	// kmsCtx, kmsCtxCancel := context.WithCancel(context.Background())
+	// defer kmsCtxCancel()
+	// kmsClient, err := google.DefaultClient(kmsCtx, "https://www.googleapis.com/auth/cloudkms")
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	kmsService, err = cloudkms.New(kmsClient)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	kmsService.UserAgent = userAgent
+	// kmsService, err = cloudkms.New(kmsClient)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+	// kmsService.UserAgent = userAgent
 
-	storageCtx, storageCtxCancel := context.WithCancel(context.Background())
-	defer storageCtxCancel()
-	storageClient, err = storage.NewClient(storageCtx,
-		option.WithUserAgent(userAgent),
-		option.WithScopes(storage.ScopeReadWrite),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// storageCtx, storageCtxCancel := context.WithCancel(context.Background())
+	// defer storageCtxCancel()
+	// storageClient, err = storage.NewClient(storageCtx,
+	// 	option.WithUserAgent(userAgent),
+	// 	option.WithScopes(storage.ScopeReadWrite),
+	// )
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	httpClient = http.Client{
 		Transport: &http.Transport{
@@ -130,6 +153,10 @@ func main() {
 			},
 		},
 	}
+
+	// Allow CTRL+C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 
 	signalCh := make(chan os.Signal)
 	signal.Notify(signalCh,
@@ -140,14 +167,16 @@ func main() {
 
 	stop := func() {
 		log.Printf("Shutting down")
-		kmsCtxCancel()
-		storageCtxCancel()
+		// kmsCtxCancel()
+		// storageCtxCancel()
 		os.Exit(0)
 	}
 
 	for {
 		select {
 		case <-signalCh:
+			stop()
+		case <-c:
 			stop()
 		default:
 		}
@@ -190,192 +219,244 @@ func main() {
 }
 
 func initialize() {
-	initRequest := InitRequest{
-		SecretShares:    5,
-		SecretThreshold: 3,
+	// initRequest := InitRequest{
+	// 	SecretShares:    5,
+	// 	SecretThreshold: 3,
+	// }
+
+	// initRequestData, err := json.Marshal(&initRequest)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+
+	// r := bytes.NewReader(initRequestData)
+	// request, err := http.NewRequest("PUT", vaultAddr+"/v1/sys/init", r)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+
+	// response, err := httpClient.Do(request)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+	// defer response.Body.Close()
+
+	// initRequestResponseBody, err := ioutil.ReadAll(response.Body)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+
+	// if response.StatusCode != 200 {
+	// 	log.Printf("init: non 200 status code: %d", response.StatusCode)
+	// 	return
+	// }
+
+	// var initResponse InitResponse
+
+	// if err := json.Unmarshal(initRequestResponseBody, &initResponse); err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+
+	// log.Println("Encrypting unseal keys and the root token...")
+
+	// Metadata
+	secretMetadata := secretMetadata{
+		name: "Secret",
+	}
+	// SecretData
+	secretData := secretData{
+		testSecret:  "testSecret",
+		testSecret2: "testSecret",
+	}
+	// Request JSON
+	k8sSecretsRequest := k8sSecretsRequest{
+		kind:       "Secret",
+		apiVersion: "v1",
+		metadata:   secretMetadata,
+		data:       secretData,
 	}
 
-	initRequestData, err := json.Marshal(&initRequest)
+	// parse JSON Data
+	k8sSecretRequestData, err := json.Marshal(&k8sSecretsRequest)
 	if err != nil {
 		log.Println(err)
-		return
+		// return
+	} else {
+		log.Println("k8sSecretRequestData PASSED")
+		log.Println(k8sSecretRequestData)
 	}
 
-	r := bytes.NewReader(initRequestData)
-	request, err := http.NewRequest("PUT", vaultAddr+"/v1/sys/init", r)
+	// POST to k8sAddr+`/api/v1/namespaces/vault-dev/secrets
+	namespace := "vault-dev"
+	k8sR := bytes.NewReader(k8sSecretRequestData)
+	k8sRequest, err := http.NewRequest("POST", k8sAddr+"/api/v1/namespaces/"+namespace+"/secrets", k8sR)
 	if err != nil {
 		log.Println(err)
-		return
+		// return
+	} else {
+		log.Println("k8sRequest PASSED")
+		log.Println(k8sRequest)
 	}
 
-	response, err := httpClient.Do(request)
+	// Response Received
+	k8sResponse, err := httpClient.Do(k8sRequest)
 	if err != nil {
 		log.Println(err)
-		return
+		// return
+	} else {
+		log.Println("k8sResponse PASSED")
+		log.Println(k8sResponse)
 	}
-	defer response.Body.Close()
+	defer k8sResponse.Body.Close()
 
-	initRequestResponseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	// rootTokenEncryptRequest := &cloudkms.EncryptRequest{
+	// 	Plaintext: base64.StdEncoding.EncodeToString([]byte(initResponse.RootToken)),
+	// }
 
-	if response.StatusCode != 200 {
-		log.Printf("init: non 200 status code: %d", response.StatusCode)
-		return
-	}
+	// rootTokenEncryptResponse, err := kmsService.Projects.Locations.KeyRings.CryptoKeys.Encrypt(kmsKeyId, rootTokenEncryptRequest).Do()
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	var initResponse InitResponse
+	// unsealKeysEncryptRequest := &cloudkms.EncryptRequest{
+	// 	Plaintext: base64.StdEncoding.EncodeToString(initRequestResponseBody),
+	// }
 
-	if err := json.Unmarshal(initRequestResponseBody, &initResponse); err != nil {
-		log.Println(err)
-		return
-	}
+	// unsealKeysEncryptResponse, err := kmsService.Projects.Locations.KeyRings.CryptoKeys.Encrypt(kmsKeyId, unsealKeysEncryptRequest).Do()
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	log.Println("Encrypting unseal keys and the root token...")
-
-	rootTokenEncryptRequest := &cloudkms.EncryptRequest{
-		Plaintext: base64.StdEncoding.EncodeToString([]byte(initResponse.RootToken)),
-	}
-
-	rootTokenEncryptResponse, err := kmsService.Projects.Locations.KeyRings.CryptoKeys.Encrypt(kmsKeyId, rootTokenEncryptRequest).Do()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	unsealKeysEncryptRequest := &cloudkms.EncryptRequest{
-		Plaintext: base64.StdEncoding.EncodeToString(initRequestResponseBody),
-	}
-
-	unsealKeysEncryptResponse, err := kmsService.Projects.Locations.KeyRings.CryptoKeys.Encrypt(kmsKeyId, unsealKeysEncryptRequest).Do()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	bucket := storageClient.Bucket(gcsBucketName)
+	// bucket := storageClient.Bucket(gcsBucketName)
 
 	// Save the encrypted unseal keys.
-	ctx := context.Background()
-	unsealKeysObject := bucket.Object("unseal-keys.json.enc").NewWriter(ctx)
-	defer unsealKeysObject.Close()
+	// ctx := context.Background()
+	// unsealKeysObject := bucket.Object("unseal-keys.json.enc").NewWriter(ctx)
+	// defer unsealKeysObject.Close()
 
-	_, err = unsealKeysObject.Write([]byte(unsealKeysEncryptResponse.Ciphertext))
-	if err != nil {
-		log.Println(err)
-	}
+	// _, err = unsealKeysObject.Write([]byte(unsealKeysEncryptResponse.Ciphertext))
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	log.Printf("Unseal keys written to gs://%s/%s", gcsBucketName, "unseal-keys.json.enc")
+	// log.Printf("Unseal keys written to gs://%s/%s", gcsBucketName, "unseal-keys.json.enc")
 
-	// Save the encrypted root token.
-	rootTokenObject := bucket.Object("root-token.enc").NewWriter(ctx)
-	defer rootTokenObject.Close()
+	// // Save the encrypted root token.
+	// rootTokenObject := bucket.Object("root-token.enc").NewWriter(ctx)
+	// defer rootTokenObject.Close()
 
-	_, err = rootTokenObject.Write([]byte(rootTokenEncryptResponse.Ciphertext))
-	if err != nil {
-		log.Println(err)
-	}
+	// _, err = rootTokenObject.Write([]byte(rootTokenEncryptResponse.Ciphertext))
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	log.Printf("Root token written to gs://%s/%s", gcsBucketName, "root-token.enc")
+	// log.Printf("Root token written to gs://%s/%s", gcsBucketName, "root-token.enc")
 
-	log.Println("Initialization complete.")
+	// log.Println("Initialization complete.")
 }
 
 func unseal() {
-	bucket := storageClient.Bucket(gcsBucketName)
+	// bucket := storageClient.Bucket(gcsBucketName)
 
-	ctx := context.Background()
-	unsealKeysObject, err := bucket.Object("unseal-keys.json.enc").NewReader(ctx)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	// need to read keys in from kubernetes
+	// ctx := context.Background()
+	// unsealKeysObject, err := bucket.Object("unseal-keys.json.enc").NewReader(ctx)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	defer unsealKeysObject.Close()
+	// defer unsealKeysObject.Close()
 
-	unsealKeysData, err := ioutil.ReadAll(unsealKeysObject)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	// unsealKeysData, err := ioutil.ReadAll(unsealKeysObject)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	unsealKeysDecryptRequest := &cloudkms.DecryptRequest{
-		Ciphertext: string(unsealKeysData),
-	}
+	// unsealKeysDecryptRequest := &cloudkms.DecryptRequest{
+	// 	Ciphertext: string(unsealKeysData),
+	// }
 
-	unsealKeysDecryptResponse, err := kmsService.Projects.Locations.KeyRings.CryptoKeys.Decrypt(kmsKeyId, unsealKeysDecryptRequest).Do()
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	// unsealKeysDecryptResponse, err := kmsService.Projects.Locations.KeyRings.CryptoKeys.Decrypt(kmsKeyId, unsealKeysDecryptRequest).Do()
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	var initResponse InitResponse
+	// var initResponse InitResponse
 
-	unsealKeysPlaintext, err := base64.StdEncoding.DecodeString(unsealKeysDecryptResponse.Plaintext)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	// unsealKeysPlaintext, err := base64.StdEncoding.DecodeString(unsealKeysDecryptResponse.Plaintext)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	if err := json.Unmarshal(unsealKeysPlaintext, &initResponse); err != nil {
-		log.Println(err)
-		return
-	}
+	// if err := json.Unmarshal(unsealKeysPlaintext, &initResponse); err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	for _, key := range initResponse.KeysBase64 {
-		done, err := unsealOne(key)
-		if done {
-			return
-		}
+	// for _, key := range initResponse.KeysBase64 {
+	// 	done, err := unsealOne(key)
+	// 	if done {
+	// 		return
+	// 	}
 
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	}
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		return
+	// 	}
+	// }
 }
 
 func unsealOne(key string) (bool, error) {
-	unsealRequest := UnsealRequest{
-		Key: key,
-	}
+	// unsealRequest := UnsealRequest{
+	// 	Key: key,
+	// }
 
-	unsealRequestData, err := json.Marshal(&unsealRequest)
-	if err != nil {
-		return false, err
-	}
+	// unsealRequestData, err := json.Marshal(&unsealRequest)
+	// if err != nil {
+	// 	return false, err
+	// }
 
-	r := bytes.NewReader(unsealRequestData)
-	request, err := http.NewRequest(http.MethodPut, vaultAddr+"/v1/sys/unseal", r)
-	if err != nil {
-		return false, err
-	}
+	// r := bytes.NewReader(unsealRequestData)
+	// request, err := http.NewRequest(http.MethodPut, vaultAddr+"/v1/sys/unseal", r)
+	// if err != nil {
+	// 	return false, err
+	// }
 
-	response, err := httpClient.Do(request)
-	if err != nil {
-		return false, err
-	}
-	defer response.Body.Close()
+	// response, err := httpClient.Do(request)
+	// if err != nil {
+	// 	return false, err
+	// }
+	// defer response.Body.Close()
 
-	if response.StatusCode != 200 {
-		return false, fmt.Errorf("unseal: non-200 status code: %d", response.StatusCode)
-	}
+	// if response.StatusCode != 200 {
+	// 	return false, fmt.Errorf("unseal: non-200 status code: %d", response.StatusCode)
+	// }
 
-	unsealRequestResponseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return false, err
-	}
+	// unsealRequestResponseBody, err := ioutil.ReadAll(response.Body)
+	// if err != nil {
+	// 	return false, err
+	// }
 
-	var unsealResponse UnsealResponse
-	if err := json.Unmarshal(unsealRequestResponseBody, &unsealResponse); err != nil {
-		return false, err
-	}
+	// var unsealResponse UnsealResponse
+	// if err := json.Unmarshal(unsealRequestResponseBody, &unsealResponse); err != nil {
+	// 	return false, err
+	// }
 
-	if !unsealResponse.Sealed {
-		return true, nil
-	}
+	// if !unsealResponse.Sealed {
+	// 	return true, nil
+	// }
 
 	return false, nil
 }
+
