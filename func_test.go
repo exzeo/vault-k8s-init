d2ea3c4da879e65a25347cea12c182820e9093be
jmitchell <jmitchell@exzeo.com>
2019-01-16 11:17:30 -0500

    changes

--- func_test.go
+++ func_test.go
@@ -1,22 +1,22 @@
-package main
+// package main
 
-import (
-	"log"
-	"testing"
-)
+// import (
+// 	"log"
+// 	"testing"
+// )
 
 // TestSetSecrets : allows you to test result of setSecrets
-func TestSetSecrets(t *testing.T) {
-	// expected := fmt.Sprintf("%s.us-east-1.s3.exzeo.com", bucket)
-	result := setSecrets()
+// func TestSetSecrets(t *testing.T) {
+// 	// expected := fmt.Sprintf("%s.us-east-1.s3.exzeo.com", bucket)
+// 	result := setSecrets()
 
-    log.Println(result)
+//     log.Println(result)
     
-	// if result == "" {
-	//     t.Fail()
-	// }
+// 	// if result == "" {
+// 	//     t.Fail()
+// 	// }
 
-	// if result != expected {
-	//     t.Errorf("Got %s, Expected: %s", result, expected)
-	// }
-}
+// 	// if result != expected {
+// 	//     t.Errorf("Got %s, Expected: %s", result, expected)
+// 	// }
+// }
