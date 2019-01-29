56e20f43cea7a948da6895f9b75e79e8fcb7ab76
jmitchell <jmitchell@exzeo.com>
2019-01-29 14:45:33 -0500

    changes

--- func_test.go
+++ func_test.go
@@ -4,40 +4,40 @@ import (
 	"testing"
 )
 
-func TestCreateSecret(t *testing.T) {
-	// secret := Secret{
-	// 	Kind:       "Secret",
-	// 	APIVersion: "v1",
-	// 	Metadata: MetaData{
-	// 		Name: "Vault Tokens",
-	// 	},
-	// 	Data: VaultToken{
-	// 		Vault_root_token: toBase64("test root"),
-	// 		Vault_token1:     toBase64("test token 1"),
-	// 		Vault_token2:     toBase64("test token 2"),
-	// 		Vault_token3:     toBase64("test token 3"),
-	// 		Vault_token4:     toBase64("test token 4"),
-	// 		Vault_token5:     toBase64("test token 5"),
-	// 	},
-	// }
+// func TestCreateSecret(t *testing.T) {
+// 	// secret := Secret{
+// 	// 	Kind:       "Secret",
+// 	// 	APIVersion: "v1",
+// 	// 	Metadata: MetaData{
+// 	// 		Name: "Vault Tokens",
+// 	// 	},
+// 	// 	Data: VaultToken{
+// 	// 		Vault_root_token: toBase64("test root"),
+// 	// 		Vault_token1:     toBase64("test token 1"),
+// 	// 		Vault_token2:     toBase64("test token 2"),
+// 	// 		Vault_token3:     toBase64("test token 3"),
+// 	// 		Vault_token4:     toBase64("test token 4"),
+// 	// 		Vault_token5:     toBase64("test token 5"),
+// 	// 	},
+// 	// }
 
-	// b, err := json.Marshal(secret)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// fmt.Println(string(b))
+// 	// b, err := json.Marshal(secret)
+// 	// if err != nil {
+// 	// 	fmt.Println(err)
+// 	// 	return
+// 	// }
+// 	// fmt.Println(string(b))
 
-	tokens := []string{}
+// 	tokens := []string{}
 
-	for index := 0; index < NumTokens; index++ {
-		tokens = append(tokens, "token")
-	}
+// 	for index := 0; index < NumTokens; index++ {
+// 		tokens = append(tokens, "token")
+// 	}
 
-	vault := VaultToken{
-		RootToken: "test-root",
-		Tokens:    tokens,
-	}
+// 	vault := VaultToken{
+// 		RootToken: "test-root",
+// 		Tokens:    tokens,
+// 	}
 
-	k8s.CreateSecret(value)
-}
+// 	k8s.CreateSecret(value)
+// }
